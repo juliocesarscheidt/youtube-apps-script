4335bd0e8fab42effd7f57708ec6b5865eba2eae
juliocesarscheidt <julio.scheidt96@gmail.com>
2022-07-03 21:15:15 -0300

    Add script

---
+++ externalApi.js
@@ -0,0 +1,31 @@
+function callApi(url, method, headers = null){
+  const options = { method, muteHttpExceptions: true, contentType: "application/json" };
+  if (headers !== null) {
+    fetchOptions.headers = headers;
+  }
+  const response = UrlFetchApp.fetch(url, options);
+  if(response.getResponseCode() != 200){
+    throw new Error(response.getContentText());
+  }
+  return JSON.parse(response.getContentText());
+}
+
+function externalApiFetch() {
+  const response = callApi('https://geo.qualaroo.com/json/', 'GET');
+  // console.log(response);
+  // { 
+  //   ip: '',
+  //   country_code: 'BR',
+  //   country_name: 'Brazil',
+  //   region_code: 'PR',
+  //   region_name: 'Parana',
+  //   city: 'Curitiba',
+  //   zip_code: '',
+  //   time_zone: 'America/Sao_Paulo',
+  //   latitude: -00.0000,
+  //   longitude: -00.0000,
+  //   metro_code: 0
+  // }
+  const { country_code, region_code, city, time_zone } = response;
+  console.log(country_code, region_code, city, time_zone);
+}
--- youtube.js
+++ youtube.js
@@ -1,13 +1,7 @@
-function callApi(url, method, headers = null){
-  const options = { method, muteHttpExceptions: true, contentType: "application/json" };
-  if (headers !== null) {
-    fetchOptions.headers = headers;
-  }
-  const response = UrlFetchApp.fetch(url, options);
-  if(response.getResponseCode() != 200){
-    throw new Error(response.getContentText());
-  }
-  return JSON.parse(response.getContentText());
+function getChannelInfo() {
+  const response = YouTube.Channels.list('snippet,contentDetails,statistics', {'mine': true});
+  const [channel] = response.items;
+  return channel;
 }
 
 function searchVideos(query, channelId, maxResults = 10) {
@@ -24,12 +18,6 @@ function searchVideos(query, channelId, maxResults = 10) {
     'title': item.snippet.title,
     'description': item.snippet.description,
   }));
-}
-
-function getChannelInfo() {
-  const response = YouTube.Channels.list('snippet,contentDetails,statistics', {'mine': true});
-  const [channel] = response.items;
-  return channel;
 }
 
 function listVideoCategories() {
@@ -88,24 +76,4 @@ function youtubeApiFetch() {
   console.log('videoDescription', videoDescription);
 
   updateVideo(videoId, videoTitle, videoDescription, categoryId);
-}
-
-function externalApiFetch() {
-  const response = callApi('https://geo.qualaroo.com/json/', 'GET');
-  // console.log(response);
-  // { 
-  //   ip: '',
-  //   country_code: 'BR',
-  //   country_name: 'Brazil',
-  //   region_code: 'PR',
-  //   region_name: 'Parana',
-  //   city: 'Curitiba',
-  //   zip_code: '',
-  //   time_zone: 'America/Sao_Paulo',
-  //   latitude: -00.0000,
-  //   longitude: -00.0000,
-  //   metro_code: 0
-  // }
-  const { country_code, region_code, city, time_zone } = response;
-  console.log(country_code, region_code, city, time_zone);
 }
